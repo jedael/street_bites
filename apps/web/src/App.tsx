30ea72cbaaaa0663c85fd8bd2b0fa782680d242d
Jean-Charles <jean-charles.rousseau@trepied.eu>
2026-03-18 16:09:54 +0100

    feat: ajout d'un badge de notification sur le panier
    - Création du composant Navigation utilisant le contexte du panier
    - Affichage d'un badge rouge circulaire avec le nombre d'articles
    - Ajout des styles CSS pour positionner le badge au-dessus du lien

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,23 +3,36 @@ import MenuPage from './pages/MenuPage'
 import CartPage from './pages/CartPage'
 import OrderConfirmationPage from './pages/OrderConfirmationPage'
 import KitchenPage from './pages/KitchenPage'
-import { CartProvider } from './context/CartContext'
+import { CartProvider, useCart } from './context/CartContext'
 import './App.css'
+
+function Navigation() {
+  const { itemCount } = useCart()
+
+  return (
+    <nav className="navbar">
+      <div className="container navbar-inner">
+        <Link to="/" className="brand">🍔 Street Bites</Link>
+        <div className="nav-links">
+          <Link to="/">Menu</Link>
+          <Link to="/cart" className="cart-link">
+            Panier
+            {itemCount > 0 && (
+              <span className="cart-badge">{itemCount}</span>
+            )}
+          </Link>
+          <Link to="/kitchen">Cuisine</Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
-        <nav className="navbar">
-          <div className="container navbar-inner">
-            <Link to="/" className="brand">🍔 Street Bites</Link>
-            <div className="nav-links">
-              <Link to="/">Menu</Link>
-              <Link to="/cart">Panier</Link>
-              <Link to="/kitchen">Cuisine</Link>
-            </div>
-          </div>
-        </nav>
+        <Navigation />
         <Routes>
           <Route path="/" element={<MenuPage />} />
           <Route path="/cart" element={<CartPage />} />
